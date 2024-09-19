aad079d2f82c1f88012fa9a4601a4276555a5368
Maya Matuszczyk <maccraft123mc@gmail.com>
2024-09-19 15:44:21 +0200

    firmware: qcom: scm: Allow QSEECOM on Lenovo Yoga Slim 7x
    BugLink: https://bugs.launchpad.net/bugs/2096827
    
    [ Upstream commit c6fa2834afc6a6fe210415ec253a61e6eafdf651 ]
    
    Allow QSEECOM on Lenovo Yoga Slim 7x, to enable accessing EFI variables.
    
    Signed-off-by: Maya Matuszczyk <maccraft123mc@gmail.com>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Link: https://lore.kernel.org/r/20240919134421.112643-2-maccraft123mc@gmail.com
    [bjorn: Rewrote commit message]
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    [koichiroden: adjusted context due to missing commit:
    6614be053588 ("firmware: qcom: scm: Allow QSEECOM on Surface Laptop 7 models")]
    Signed-off-by: Koichiro Den <koichiro.den@canonical.com>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -1740,6 +1740,7 @@ EXPORT_SYMBOL_GPL(qcom_scm_qseecom_app_send);
 static const struct of_device_id qcom_scm_qseecom_allowlist[] __maybe_unused = {
 	{ .compatible = "lenovo,flex-5g" },
 	{ .compatible = "lenovo,thinkpad-x13s", },
+	{ .compatible = "lenovo,yoga-slim7x" },
 	{ .compatible = "qcom,sc8180x-primus" },
 	{ .compatible = "qcom,x1e80100-crd" },
 	{ .compatible = "qcom,x1e80100-qcp" },
