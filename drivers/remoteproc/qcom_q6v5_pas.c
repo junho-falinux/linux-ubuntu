19c684f1a6aaf31b91354c3c7b27ba78f47d869c
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2024-10-27 01:09:45 +0300

    remoteproc: qcom: pas: enable SAR2130P audio DSP support
    BugLink: https://bugs.launchpad.net/bugs/2096827
    
    [ Upstream commit 009e288c989b3fe548a45c82da407d7bd00418a9 ]
    
    Enable support for the Audio DSP on the Qualcomm SAR2130P platform,
    reusing the SM8350 resources.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Reviewed-by: Neil Armstrong <neil.armstrong@linaro.org>
    Link: https://lore.kernel.org/r/20241027-sar2130p-adsp-v1-3-bd204e39d24e@linaro.org
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    [koichiroden: adjusted context due to missing commit:
    9091225ba28c ("remoteproc: qcom: pas: Add support for SA8775p ADSP, CDSP and GPDSP")]
    Signed-off-by: Koichiro Den <koichiro.den@canonical.com>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -1341,6 +1341,7 @@ static const struct of_device_id adsp_of_match[] = {
 	{ .compatible = "qcom,qcs404-adsp-pas", .data = &adsp_resource_init },
 	{ .compatible = "qcom,qcs404-cdsp-pas", .data = &cdsp_resource_init },
 	{ .compatible = "qcom,qcs404-wcss-pas", .data = &wcss_resource_init },
+	{ .compatible = "qcom,sar2130p-adsp-pas", .data = &sm8350_adsp_resource},
 	{ .compatible = "qcom,sc7180-adsp-pas", .data = &sm8250_adsp_resource},
 	{ .compatible = "qcom,sc7180-mpss-pas", .data = &mpss_resource_init},
 	{ .compatible = "qcom,sc7280-adsp-pas", .data = &sm8350_adsp_resource},
