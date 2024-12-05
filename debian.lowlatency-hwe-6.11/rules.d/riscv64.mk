161b9e46f85e4666cdf1280859557c8512fb7eca
Stefan Bader <stefan.bader@canonical.com>
2024-12-05 16:59:34 +0100

    UBUNTU: [Packaging] Initial noble:linux-lowlatency-hwe-6.11 setup
    Ignore: yes
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

---
+++ debian.lowlatency-hwe-6.11/rules.d/riscv64.mk
@@ -0,0 +1,16 @@
+human_arch	= RISC-V
+build_arch	= riscv
+defconfig	= defconfig
+flavours        = generic
+build_image	= Image
+kernel_file	= arch/$(build_arch)/boot/Image
+install_file	= vmlinuz
+
+vdso		= vdso_install
+no_dumpfile	= true
+
+do_flavour_image_package = false
+do_tools	= false
+do_extras_package = false
+do_flavour_header_package = false
+do_common_headers_indep = false
