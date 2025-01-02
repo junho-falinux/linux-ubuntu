ea3e968ae6d99951c7b30fe865e1c583eb8caa4d
Simon Trimmer <simont@opensource.cirrus.com>
2025-01-02 20:33:34 +0800

    ASoC: Intel: sof_sdw: Fix DMI match for Lenovo 83LC
    BugLink: https://bugs.launchpad.net/bugs/2103829
    
    [ Upstream commit c9e05763f334845ba69494dd71d7cbfd05fd0e6e ]
    
    Update the DMI match for a Lenovo laptop to the new DMI identifier.
    
    This laptop ships with a different DMI identifier to what was expected,
    and also has the DMICs connected to the host rather than the cs42l43
    codec.
    
    Signed-off-by: Simon Trimmer <simont@opensource.cirrus.com>
    Fixes: 83c062ae81e8 ("ASoC: Intel: sof_sdw: Add quirks for some new Lenovo laptops")
    Reviewed-by: Liam Girdwood <liam.r.girdwood@intel.com>
    Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Link: https://patch.msgid.link/20250102123335.256698-2-yung-chuan.liao@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    [nwager: adjusted macro naming without backporting the patch series that
    includes commit:
    408a454ee888 ("ASoC: intel: rename soundwire common header macros")]
    Signed-off-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- sound/soc/intel/boards/sof_sdw.c
+++ sound/soc/intel/boards/sof_sdw.c
@@ -551,9 +551,9 @@ static const struct dmi_system_id sof_sdw_quirk_table[] = {
 		.callback = sof_sdw_quirk_cb,
 		.matches = {
 			DMI_MATCH(DMI_SYS_VENDOR, "LENOVO"),
-			DMI_EXACT_MATCH(DMI_PRODUCT_SKU, "3832")
+			DMI_MATCH(DMI_PRODUCT_NAME, "83LC")
 		},
-		.driver_data = (void *)(SOF_SIDECAR_AMPS),
+		.driver_data = (void *)(SOF_SIDECAR_AMPS | SOF_CODEC_MIC),
 	},
 	{
 		.callback = sof_sdw_quirk_cb,
