c155b88b80f7ee4f25f278300261507ce3eef0bd
Konstantin Andreev <andreev@swemel.ru>
2025-01-17 19:36:42 +0300

    smack: dont compile ipv6 code unless ipv6 is configured
    BugLink: https://bugs.launchpad.net/bugs/2110173
    
    [ Upstream commit bfcf4004bcbce2cb674b4e8dbd31ce0891766bac ]
    
    I want to be sure that ipv6-specific code
    is not compiled in kernel binaries
    if ipv6 is not configured.
    
    [1] was getting rid of "unused variable" warning, but,
    with that, it also mandated compilation of a handful ipv6-
    specific functions in ipv4-only kernel configurations:
    
    smk_ipv6_localhost, smack_ipv6host_label, smk_ipv6_check.
    
    Their compiled bodies are likely to be removed by compiler
    from the resulting binary, but, to be on the safe side,
    I remove them from the compiler view.
    
    [1]
    Fixes: 00720f0e7f28 ("smack: avoid unused 'sip' variable warning")
    
    Signed-off-by: Konstantin Andreev <andreev@swemel.ru>
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- security/smack/smack_lsm.c
+++ security/smack/smack_lsm.c
@@ -2531,6 +2531,7 @@ static struct smack_known *smack_ipv4host_label(struct sockaddr_in *sip)
 	return NULL;
 }
 
+#if IS_ENABLED(CONFIG_IPV6)
 /*
  * smk_ipv6_localhost - Check for local ipv6 host address
  * @sip: the address
@@ -2598,6 +2599,7 @@ static struct smack_known *smack_ipv6host_label(struct sockaddr_in6 *sip)
 
 	return NULL;
 }
+#endif /* CONFIG_IPV6 */
 
 /**
  * smack_netlbl_add - Set the secattr on a socket
@@ -2708,6 +2710,7 @@ static int smk_ipv4_check(struct sock *sk, struct sockaddr_in *sap)
 	return rc;
 }
 
+#if IS_ENABLED(CONFIG_IPV6)
 /**
  * smk_ipv6_check - check Smack access
  * @subject: subject Smack label
@@ -2740,6 +2743,7 @@ static int smk_ipv6_check(struct smack_known *subject,
 	rc = smk_bu_note("IPv6 check", subject, object, MAY_WRITE, rc);
 	return rc;
 }
+#endif /* CONFIG_IPV6 */
 
 #ifdef SMACK_IPV6_PORT_LABELING
 /**
@@ -3072,7 +3076,9 @@ static int smack_socket_connect(struct socket *sock, struct sockaddr *sap,
 		return 0;
 	if (addrlen < offsetofend(struct sockaddr, sa_family))
 		return 0;
-	if (IS_ENABLED(CONFIG_IPV6) && sap->sa_family == AF_INET6) {
+
+#if IS_ENABLED(CONFIG_IPV6)
+	if (sap->sa_family == AF_INET6) {
 		struct sockaddr_in6 *sip = (struct sockaddr_in6 *)sap;
 		struct smack_known *rsp = NULL;
 
@@ -3092,6 +3098,8 @@ static int smack_socket_connect(struct socket *sock, struct sockaddr *sap,
 
 		return rc;
 	}
+#endif /* CONFIG_IPV6 */
+
 	if (sap->sa_family != AF_INET || addrlen < sizeof(struct sockaddr_in))
 		return 0;
 	rc = smk_ipv4_check(sock->sk, (struct sockaddr_in *)sap);
