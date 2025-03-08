7b270df818ec12379d4ae4ccfa2d450dc4a3a678
Bibo Mao <maobibo@loongson.cn>
2025-03-08 13:52:04 +0800

    LoongArch: KVM: Fix GPA size issue about VM
    BugLink: https://bugs.launchpad.net/bugs/2107437
    
    commit 6bdbb73dc8d99fbb77f5db79dbb6f108708090b4 upstream.
    
    Physical address space is 48 bit on Loongson-3A5000 physical machine,
    however it is 47 bit for VM on Loongson-3A5000 system. Size of physical
    address space of VM is the same with the size of virtual user space (a
    half) of physical machine.
    
    Variable cpu_vabits represents user address space, kernel address space
    is not included (user space and kernel space are both a half of total).
    Here cpu_vabits, rather than cpu_vabits - 1, is to represent the size of
    guest physical address space.
    
    Also there is strict checking about page fault GPA address, inject error
    if it is larger than maximum GPA address of VM.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [nwager: Context change due to missing commit:
    cdc118f802410 ("LoongArch: KVM: Enable paravirt feature control from VMM")]
    Signed-off-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- arch/loongarch/kvm/vm.c
+++ arch/loongarch/kvm/vm.c
@@ -39,7 +39,12 @@ int kvm_arch_init_vm(struct kvm *kvm, unsigned long type)
 	spin_lock_init(&kvm->arch.phyid_map_lock);
 
 	kvm_init_vmcs(kvm);
-	kvm->arch.gpa_size = BIT(cpu_vabits - 1);
+
+	/*
+	 * cpu_vabits means user address space only (a half of total).
+	 * GPA size of VM is the same with the size of user address space.
+	 */
+	kvm->arch.gpa_size = BIT(cpu_vabits);
 	kvm->arch.root_level = CONFIG_PGTABLE_LEVELS - 1;
 	kvm->arch.invalid_ptes[0] = 0;
 	kvm->arch.invalid_ptes[1] = (unsigned long)invalid_pte_table;
