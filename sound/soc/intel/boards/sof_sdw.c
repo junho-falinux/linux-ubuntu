ekuiter/torte-linux

ASoC: Intel: sof_sdw: Add quirk for Asus Zenbook S14
Asus laptops with sound PCI subsystem ID 1043:1e13 have the DMICs
connected to the host instead of the CS42L43 so need the
SOC_SDW_CODEC_MIC quirk.

Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>
Reviewed-by: Liam Girdwood <liam.r.girdwood@intel.com>
Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
Link: https://patch.msgid.link/20250204053943.93596-3-yung-chuan.liao@linux.intel.com
Signed-off-by: Mark Brown <broonie@kernel.org>

sound/soc/intel/boards/sof_sdw.c
@@ -753,6 +753,7 @@ static const struct dmi_system_id sof_sdw_quirk_table[] = {
 };
 
 static const struct snd_pci_quirk sof_sdw_ssid_quirk_table[] = {
+	SND_PCI_QUIRK(0x1043, 0x1e13, "ASUS Zenbook S14", SOC_SDW_CODEC_MIC),
 	{}
 };
 

junho-falinux/linux-ubuntu

BugLink: https://bugs.launchpad.net/bugs/2107522
[ Upstream commit 0843449708085c4fb45a3c325c2fbced556f6abf ]
Signed-off-by: Sasha Levin <sashal@kernel.org>
[nwager: adjusted macro naming without backporting the patch series that
includes commit:
408a454ee888 ("ASoC: intel: rename soundwire common header macros")]
Signed-off-by: Noah Wager <noah.wager@canonical.com>
Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

sound/soc/intel/boards/sof_sdw.c
@@ -618,6 +618,7 @@ static const struct dmi_system_id sof_sdw_quirk_table[] = {
 };
 
 static const struct snd_pci_quirk sof_sdw_ssid_quirk_table[] = {
+	SND_PCI_QUIRK(0x1043, 0x1e13, "ASUS Zenbook S14", SOF_CODEC_MIC),
 	{}
 };
 