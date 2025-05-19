b317fe27f36ed768117ffc8fc912b82eb1e75aae
David Howells <dhowells@redhat.com>
2025-05-19 10:07:03 +0100

    netfs: Fix the request's work item to not require a ref
    BugLink: https://bugs.launchpad.net/bugs/2119603
    
    [ Upstream commit 20d72b00ca814d748f5663484e5c53bb2bf37a3a ]
    
    When the netfs_io_request struct's work item is queued, it must be supplied
    with a ref to the work item struct to prevent it being deallocated whilst
    on the queue or whilst it is being processed.  This is tricky to manage as
    we have to get a ref before we try and queue it and then we may find it's
    already queued and is thus already holding a ref - in which case we have to
    try and get rid of the ref again.
    
    The problem comes if we're in BH or IRQ context and need to drop the ref:
    if netfs_put_request() reduces the count to 0, we have to do the cleanup -
    but the cleanup may need to wait.
    
    Fix this by adding a new work item to the request, ->cleanup_work, and
    dispatching that when the refcount hits zero.  That can then synchronously
    cancel any outstanding work on the main work item before doing the cleanup.
    
    Adding a new work item also deals with another problem upstream where it's
    sometimes changing the work func in the put function and requeuing it -
    which has occasionally in the past caused the cleanup to happen
    incorrectly.
    
    As a bonus, this allows us to get rid of the 'was_async' parameter from a
    bunch of functions.  This indicated whether the put function might not be
    permitted to sleep.
    
    Fixes: 3d3c95046742 ("netfs: Provide readahead and readpage netfs helpers")
    Signed-off-by: David Howells <dhowells@redhat.com>
    Link: https://lore.kernel.org/20250519090707.2848510-4-dhowells@redhat.com
    cc: Paulo Alcantara <pc@manguebit.com>
    cc: Marc Dionne <marc.dionne@auristor.com>
    cc: Steve French <stfrench@microsoft.com>
    cc: linux-cifs@vger.kernel.org
    cc: netfs@lists.linux.dev
    cc: linux-fsdevel@vger.kernel.org
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Manuel Diewald <manuel.diewald@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- net/9p/client.c
+++ net/9p/client.c
@@ -1699,7 +1699,7 @@ p9_client_write_subreq(struct netfs_io_subrequest *subreq)
 				    start, len, &subreq->io_iter);
 	}
 	if (IS_ERR(req)) {
-		netfs_write_subrequest_terminated(subreq, PTR_ERR(req), false);
+		netfs_write_subrequest_terminated(subreq, PTR_ERR(req));
 		return;
 	}
 
@@ -1707,7 +1707,7 @@ p9_client_write_subreq(struct netfs_io_subrequest *subreq)
 	if (err) {
 		trace_9p_protocol_dump(clnt, &req->rc);
 		p9_req_put(clnt, req);
-		netfs_write_subrequest_terminated(subreq, err, false);
+		netfs_write_subrequest_terminated(subreq, err);
 		return;
 	}
 
@@ -1719,7 +1719,7 @@ p9_client_write_subreq(struct netfs_io_subrequest *subreq)
 	p9_debug(P9_DEBUG_9P, "<<< RWRITE count %d\n", len);
 
 	p9_req_put(clnt, req);
-	netfs_write_subrequest_terminated(subreq, written, false);
+	netfs_write_subrequest_terminated(subreq, written);
 }
 EXPORT_SYMBOL(p9_client_write_subreq);
 
